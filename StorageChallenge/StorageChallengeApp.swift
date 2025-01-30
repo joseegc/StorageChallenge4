65d3f569e7e87560334a60a693a9c5eb941c9bb1
José Elias <joseelias5589@gmail.com>
2025-01-30 15:09:35 -0300

    Initial Commit

---
+++ StorageChallenge/StorageChallengeApp.swift
@@ -0,0 +1,20 @@
+//
+//  StorageChallengeApp.swift
+//  StorageChallenge
+//
+//  Created by JOSE ELIAS GOMES CAMARGO on 30/01/25.
+//
+
+import SwiftUI
+
+@main
+struct StorageChallengeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
