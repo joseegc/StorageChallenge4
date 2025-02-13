ba2f43dcff4dc11d54e26db5d21265959e1b0192
José Elias <joseelias5589@gmail.com>
2025-02-13 16:17:26 -0300

    teste

---
+++ StorageChallenge/View/Cliente/mudarDeBanco.swift
@@ -0,0 +1,18 @@
+//
+//  mudarDeBanco.swift
+//  StorageChallenge
+//
+//  Created by EDSON DE OLIVEIRA CORREIA on 13/02/25.
+//
+
+import SwiftUI
+
+struct mudarDeBanco: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mudarDeBanco()
+}
