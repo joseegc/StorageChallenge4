4064d5796bf807b599300969d7cd5dcb59bfc443
José Elias <joseelias5589@gmail.com>
2025-02-14 15:36:22 -0300

    correcoes em pedidos

--- StorageChallenge/View/Cliente/CadastrarEditarClienteView.swift
+++ StorageChallenge/View/Cliente/CadastrarEditarClienteView.swift
@@ -1,15 +1,8 @@
-//
-//  CadastrarClienteView.swift
-//  StorageChallenge
-//
-//  Created by JOSE ELIAS GOMES CAMARGO on 31/01/25.
-//
-
 import SwiftUI
 import PhotosUI
 
 struct CadastrarEditarClienteView: View {
-    @EnvironmentObject var viewModel: ClienteViewModel
+    @EnvironmentObject var clientesViewModel: ClienteViewModel
     @Environment(\.presentationMode) var presentationMode
     @Environment(\.colorScheme) var colorScheme
     
@@ -17,7 +10,7 @@ struct CadastrarEditarClienteView: View {
     @State var photosPickerItem: PhotosPickerItem?
     
     @State var clienteInput = Cliente()
-//    var idDoCliente: UUID?
+    var idDoCliente: UUID?
     
     @State var medidas :[Medida] = []
     @State var telefoneValido: Bool = true
@@ -122,7 +115,7 @@ struct CadastrarEditarClienteView: View {
                             telefoneValido = true
                         }
                     }
-                        
+                    
                     .keyboardType(.numberPad)
                     Rectangle().fill(Color("cinzaEscuro")).frame(height: 1)
                     if (!telefoneValido) {
@@ -139,11 +132,11 @@ struct CadastrarEditarClienteView: View {
                     }
                     
                     
-//                    HStack {
-//                        Text("Descrição")
-//                        Spacer()
-//                        Text("Medida").padding(.trailing, 55)
-//                    }
+                    //                    HStack {
+                    //                        Text("Descrição")
+                    //                        Spacer()
+                    //                        Text("Medida").padding(.trailing, 55)
+                    //                    }
                     
                     // Exibe as medidas
                     ForEach($clienteInput.medidas) { $medida in
@@ -182,7 +175,7 @@ struct CadastrarEditarClienteView: View {
                                 var idMedida = medida.id
                                 if let index = clienteInput.medidas.firstIndex(where: { $0.id == idMedida }) {
                                     clienteInput.medidas.remove(at: index)
-                                    viewModel.deletarMedida(id: idMedida)
+                                    clientesViewModel.deletarMedida(id: idMedida)
                                 }
                             },
                                    label: {
@@ -219,17 +212,26 @@ struct CadastrarEditarClienteView: View {
             .background(Color(colorScheme == .dark ? Color("pretoFixo"): Color("cinzaClaro")))
             .cornerRadius(20)
             .padding(.horizontal, 21)
-            .navigationTitle(viewModel.cliente.id != nil ? "Editar Cliente" : "Cadastrar Cliente")
+            .navigationTitle(idDoCliente != nil ? "Editar Cliente" : "Cadastrar Cliente")
             .navigationBarTitleDisplayMode(.inline)
             .task {
-                if viewModel.cliente.id != nil {
-                    clienteInput = viewModel.cliente
+                if idDoCliente != nil {
+                    clienteInput = clientesViewModel.buscarClientePorId(id: idDoCliente!)!
+                    //                    clientesViewModel.buscarClientePorId(idDoCliente: idDoCliente!)
+                    //                    if let foto = clientesViewModel.cliente.foto {
+                    //                        imagem = UIImage(data: foto)
+                    //                    }
+                    //
+                    
                     
                     if let imageData =  clienteInput.foto {
                         imagem =  UIImage(data: imageData)
                         
                     }
-                }           }
+                }
+                //                else {
+                //                    clientesViewModel.cliente = Cliente()
+                //                }
             }
             
             .toolbar {
@@ -241,20 +243,22 @@ struct CadastrarEditarClienteView: View {
                             }
                             
                             //                        clienteInput.medidas = medidas
-//                            viewModel.cliente.medidas = clienteInput.medidas
-//                            
+                            clientesViewModel.cliente.medidas = clienteInput.medidas
                             
-                            viewModel.cliente = clienteInput
-//                            print(viewModel.cliente.medidas)
-                            if viewModel.cliente.id != nil {
-                                viewModel.editarCliente()
+                            
+                            clientesViewModel.cliente = clienteInput
+                            print(clientesViewModel.cliente.medidas)
+                            if idDoCliente != nil {
+                                clientesViewModel.editarCliente()
                             } else {
-                                viewModel.salvarCliente()
+                                clientesViewModel.salvarCliente()
                                 print("Adicionando ao banco")
                                 
                             }
                             
-//                            clientesViewModel.buscarTodosClientes()
+                            
+                            
+                            clientesViewModel.buscarTodosClientes()
                             presentationMode.wrappedValue.dismiss()
                         }
                     }) {
